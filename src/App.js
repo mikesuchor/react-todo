8fffc1d9420da633832e4a8c001cbd2d9cb2a724
mikesuchor <mikesuchor@gmail.com>
2019-01-27 23:44:06 -0700

    Implemented basic Todo List

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import TodoList from './TodoList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: '',
+      items: [] 
+    }
+  }
+
+  onChange = (event) => {
+    this.setState({todo: event.target.value});
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      todo: '',
+      items: [...this.state.items, this.state.todo]
+    });
+  }
+  
   render() {
     return (
-      <div className="todo">
-        <form>
-          <input></input>
+      <div>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input value={this.state.todo} onChange={this.onChange}></input>
           <button>Add Task</button>
         </form>
+        <TodoList items={this.state.items} />
       </div>
     );
   }
