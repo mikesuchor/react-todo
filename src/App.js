28f7e98e3c2f24e13ffda0946f9e2ac6f448b021
mikesuchor <mikesuchor@gmail.com>
2019-02-08 22:03:49 -0800

    Removed .js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import TodoList from './TodoList';
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
