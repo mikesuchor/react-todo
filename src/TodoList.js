bf8742e681facd1b5f47d9cd9984ece24758cc46
mikesuchor <mikesuchor@gmail.com>
2019-03-19 10:23:26 -0600

    Updated todo list

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,63 @@
-import React from 'react';
-import './TodoList.css';
+import React from "react"; 
+import TodoItems from "./TodoItems";
+import "./TodoList.css";
 
-class TodoList extends React.Component {
-    render() {
-        return (
-            <ul>
-            {
-                this.props.items.map((item) => <li key={item.toString()}>{item}</li>)
-            }
-         </ul>
-        )
-    }
-}
+class TodoList extends React.Component { 
+  constructor(props) {    
+    super(props);     
+
+    this.state = {    
+      items: []  
+    };
+  }
+
+  addItem = (event) => {
+    let itemArray = this.state.items;
+
+    if (this._inputElement.value !== "") {    
+      itemArray.unshift(
+        {      
+          text: this._inputElement.value,      
+          key: Date.now()    
+        }
+      );
+
+      this.setState({      
+        items: itemArray    
+      });   
+
+      this._inputElement.value = "";  
+    }   
+    event.preventDefault();
+  }
+
+  deleteItem = (key) => {  
+    let filteredItems = this.state.items.filter(function (item) {    
+      return (item.key !== key);  
+    }); 
+
+    this.setState({    
+      items: filteredItems  
+    });
+  }
+
+  render() {    
+    return (      
+      <div className="todoListMain">        
+        <div className="header">          
+          <form onSubmit={this.addItem}>            
+            <input ref={(input) => this._inputElement = input} 
+              placeholder="enter task">
+            </input>            
+            <button type="submit">add</button>          
+          </form>        
+        </div>
+
+        <TodoItems entries={this.state.items} 
+                  delete={this.deleteItem}/>     
+      </div>    
+    );  
+  }
+} 
 
 export default TodoList;
